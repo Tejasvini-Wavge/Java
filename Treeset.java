b98bb645434d2457ea6b8c07a75d2f57929c51bf
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2024-10-01 14:34:59 +0530

    Add files via upload

---
+++ Treeset.java
@@ -0,0 +1,15 @@
+import java.util.*;
+    public class TreeSetDemo
+{
+    public static void main(String args[]) 
+{
+ TreeSet ts=new TreeSet();u
+     ts.add("Z");
+     ts.add("A");
+     ts.add("B");
+     ts.add("E");
+     ts.add("S");
+     ts.add("V");
+     System.out.println(ts);
+}
+}
